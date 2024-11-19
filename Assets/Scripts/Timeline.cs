d13a501b44810c2cfa5bc2f0607acbd4f7fc54a6
evaroderick23 <evaroderick23@gmail.com>
2024-11-19 19:48:19 -0400

    Finished timeline events

---
+++ Assets/Scripts/Timeline.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Timeline : MonoBehaviour
+{
+
+    public PlayableDirector timeline;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timeline.Play();
+    }
+}
